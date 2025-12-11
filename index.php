600b04ebc5cba03c0a84f42dee64d87874f53fcc
QuintenNolting <203368@novacollege.nl>
2025-12-11 09:18:12 +0100

    Add website files

---
+++ index.php
@@ -0,0 +1,135 @@
+<head>
+    <title>Highscores Airhockey</title>
+    <link rel="stylesheet" href="css/csswebsiteairhockey.css">
+</head>
+
+
+<body>
+<div class="Background">
+    <img src="assets/ChatGPT%20Image%2010%20dec%202025,%2012_51_26.png" width="122%" id="Backgroundback">
+</div>
+
+
+<div class="Title">
+    <img src="assets/placeholdersquare.jpg" width="100%" height="100%">
+</div>
+
+<div class="Highscores5Mins">
+    <div class="HighscoresToppart">
+        <img src="assets/placeholdersquare.jpg" width="100%" height="20%">
+    </div>
+    <br>
+    <br>
+    <div class="Tablesize">
+        <div class="Highscore5Mins">
+            Round Duration 5 Minutes<br><br>
+        <table id="Tabletextcolor">
+    <tr><td>Playername</td><td>Points</td><td>Date</td></tr>
+    <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+    <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+    <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+    <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+    <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+    <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+    <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+    <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+    <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+    <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+
+    </table></div></div>
+    <h4><span id="Tableunderline">~~~~~~~~~~~~~~~~~~~~~~~~~</span></h4>
+</div>
+
+<div class="Highscores10Mins">
+    <div class="HighscoresToppart">
+        <img src="assets/placeholdersquare.jpg" width="100%" height="20%">
+    </div>
+    <br>
+    <br>
+    <div class="Tablesize">
+        <div class="Highscore10Mins">
+            Round Duration 10 Minutes<br><br>
+            <table id="Tabletextcolor">
+                <tr><td>Playername</td><td>Points</td><td>Date</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+
+            </table></div></div>
+    <h4><span id="Tableunderline">~~~~~~~~~~~~~~~~~~~~~~~~~</span></h4>
+</div>
+
+<div class="Highscores15Mins">
+    <div class="HighscoresToppart">
+        <img src="assets/placeholdersquare.jpg" width="100%" height="20%">
+    </div>
+    <br>
+    <br>
+    <div class="Tablesize">
+        <div class="Highscore15Mins">
+            Round Duration 15 Minutes<br><br>
+            <table id="Tabletextcolor">
+                <tr><td>Playername</td><td>Points</td><td>Date</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+                <tr><td>Eeeeeeblaw</td><td>gel</td><td>morg</td></tr>
+
+            </table></div></div>
+    <h4><span id="Tableunderline">~~~~~~~~~~~~~~~~~~~~~~~~~</span></h4>
+</div>
+
+
+<div class="Singleplayer">
+    <img src="assets/placeholdersquare.jpg" width="100%" height="100%">
+</div>
+<div class="Multiplayer">
+    <img src="assets/placeholdersquare.jpg" width="100%" height="100%">
+</div>
+<div class="Double-Ball">
+    <img src="assets/placeholdersquare.jpg" width="100%" height="100%">
+</div>
+<div class="Obstacles">
+    <img src="assets/placeholdersquare.jpg" width="100%" height="100%">
+</div>
+<div class="Two-vs-Two">
+    <img src="assets/placeholdersquare.jpg" width="100%" height="100%">
+</div>
+<div class="Free-For-All">
+    <img src="assets/placeholdersquare.jpg" width="100%" height="100%">
+</div>
+
+
+<div class="SidebarLeft">
+    <img src="assets/redsidebar.webp"
+         alt="Moving image" class="SidebarLeftMoving"
+</div>
+<div class="SidebarLeft2">
+    <img src="assets/redsidebar.webp"
+         alt="Moving image" class="SidebarLeftMoving2"
+</div>
+
+
+<div class="SidebarRight">
+    <img src="assets/bluesidebar.webp"
+         alt="Moving image" class="SidebarRightMoving"
+</div>
+<div class="SidebarRight2">
+    <img src="assets/bluesidebar.webp"
+         alt="Moving image" class="SidebarRightMoving2"
+</div>
+
+</body>
